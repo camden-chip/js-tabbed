96e763b3f28ae0554152a9d4fc44b7e40e23db16
Tatsiana Chypsanava <camden_chip@hotmail.com>
2016-04-19 09:58:33 +1200

    added javascript to tabs

--- js/tab.js
+++ js/tab.js
@@ -1,1 +1,46 @@
-alert('hello');
+
+//find all the buttons
+var buttonOne = document.querySelector ('#button-one');
+var buttonTwo = document.querySelector ('#button-two');
+var buttonThree = document.querySelector ('#button-three');
+var buttonFour = document.querySelector ('#button-four');
+var buttonFive = document.querySelector ('#button-five');
+//Find all sections
+var allSections = document.querySelectorAll('.tab');
+//attach click event listeners to the buttons
+buttonOne.onclick = changeTab;
+buttonTwo.onclick = changeTab;
+buttonThree.onclick = changeTab;
+buttonFour.onclick = changeTab;
+buttonFive.onclick = changeTab;
+//function that change the visibale tab
+function changeTab() {
+//hide all tabs
+for(var i=0; i<allSections.length; i++ ) {
+allSections[i].style.display = 'none';
+}
+//Switch based on the ID of the button clicked
+switch( this.id ) {
+	case 'button-one':
+allSections[0].style.display = 'block';
+	break;
+
+
+case 'button-two':
+allSections[1].style.display = 'block';
+	break;
+
+case 'button-three':
+allSections[2].style.display = 'block';
+	break;
+
+case 'button-four':
+allSections[3].style.display = 'block';
+	break;
+
+case 'button-five':
+allSections[4].style.display = 'block';
+	break;
+
+} 
+}
